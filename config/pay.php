21e0611c1531f05b02bd2f5cef5963a53c767168
Taylor <taylor@itcht.com>
2018-11-13 10:22:14 +0800

    集成支付宝

--- config/pay.php
+++ config/pay.php
@@ -2,20 +2,20 @@
 
 return [
     'alipay' => [
-        'app_id'         => '',
-        'ali_public_key' => '',
-        'private_key'    => '',
+        'app_id'         => env('ALIPAY_APP_ID', ''),
+        'ali_public_key' => env('ALIPAY_PUBLIC_KEY', ''),
+        'private_key'    => env('ALIPAY_PRIVATE_KEY', ''),
         'log'            => [
             'file' => storage_path('logs/alipay.log'),
         ],
     ],
 
     'wechat' => [
-        'app_id'      => '',
-        'mch_id'      => '',
-        'key'         => '',
-        'cert_client' => '',
-        'cert_key'    => '',
+        'app_id'      => env('WECHATPAY_APP_ID', ''),
+        'mch_id'      => env('WECHATPAY_MCH_ID', ''),
+        'key'         => env('WECHATPAY_KEY', ''),
+        'cert_client' => env('WECHATPAY_CERT_CLIENT', ''),
+        'cert_key'    => env('WECHATPAY_CERT_KEY', ''),
         'log'         => [
             'file' => storage_path('logs/wechat_pay.log'),
         ],
